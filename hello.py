ee0f7faec289a2e92e95147cb13740ba5eb42275
akramahmed <akram.ahmed18@my.northampton.ac.uk>
2024-02-03 17:29:02 -0500

    Added new function

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
 def say_hello():
     print("Hello, World!")
 
+def greet_person(name):
+    print(f"Class, {name}!")
+
 say_hello()
+greet_person("2024W-T3 AISC2013 - Deployment of AI Solutions 01 (M07 Group 1)")
